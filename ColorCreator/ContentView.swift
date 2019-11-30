e2ab2029655749cf1ebbdaad94cee28c494fa84b
Ron Olson <tachoknight@gmail.com>
2019-11-30 10:38:36 -0500

    Initial Commit

---
+++ ColorCreator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ColorCreator
+//
+//  Created by Ron Olson on 11/30/19.
+//  Copyright © 2019 Ron Olson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
